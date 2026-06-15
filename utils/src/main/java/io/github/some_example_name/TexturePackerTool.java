f1428675f8c5be3931c6afa5b6be0ac23a8a18fb
whatsdespair <mariaalessiaspataru@gmail.com>
2026-06-15 20:22:02 +0300

    small changes

--- utils/src/main/java/io/github/some_example_name/TexturePackerTool.java
+++ utils/src/main/java/io/github/some_example_name/TexturePackerTool.java
@@ -5,9 +5,9 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 public class TexturePackerTool {
 
     public static void main(String[] args) {
-        String inputDir = "";
-        String outputDir = "";
-        String packFileName = "";
+        String inputDir = "assets_raw/objects";
+        String outputDir = "assets/graphics";
+        String packFileName = "objects";
 
         TexturePacker.process(inputDir, outputDir, packFileName);
     }
